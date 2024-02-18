f45b92a10e5b78b4c358f3b5cb3b94353469ca56
sufgaz <sufgaz911@gmail.com>
2024-02-18 19:30:31 +0000

    added jsx pages and css pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <header>
+    <ul class="nav justify-content-end">
+  <li class="nav-item item1">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item item2">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item item3">
+    <a class="nav-link" href="#">link</a>
+  </li>
+</ul>
+    </header>
+      <h1>Welcome to my Portfolio</h1>
       <div className="card">
 
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hi, I'm Sufyan, a student at the EDX bootcamp, I have attached previous work that I have acheived as a student in the projects page.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
+
 
 export default App
